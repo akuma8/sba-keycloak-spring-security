0339c3b12c420ca1367953f51bc6781e9030d9f2
Attoumane Ahamadi <akuma8@hotmail.com>
2020-09-15 20:09:48 +0200

    Finally found solution to extract roles from the access token

--- src/main/java/com/akuma/configuration/SecurityConfig.java
+++ src/main/java/com/akuma/configuration/SecurityConfig.java
@@ -3,21 +3,25 @@ package com.akuma.configuration;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
-import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
-import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.OAuth2AccessToken;
-import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
-import org.springframework.security.oauth2.core.oidc.user.OidcUser;
-import org.springframework.security.oauth2.core.oidc.user.OidcUserAuthority;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Value("${spring.security.oauth2.client.provider.keycloak.issuer-uri}")
+    private String issuer;
 
     @Override
     protected void configure( HttpSecurity http ) throws Exception {
@@ -28,22 +32,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().hasRole( "ACTUATOR" )
                 .and()
                 .oauth2Login( oauth2 -> oauth2.userInfoEndpoint(
-                        userInfo -> userInfo.oidcUserService( this.oidcUserService() ) ) );
+                        userInfo -> userInfo.userService( this.oidcUserService() ) ) );
     }
 
 
-    private OAuth2UserService<OidcUserRequest, OidcUser> oidcUserService() {
-        final OidcUserService delegate = new OidcUserService();
+    private OAuth2UserService<OAuth2UserRequest, OAuth2User> oidcUserService() {
+        final DefaultOAuth2UserService delegate = new DefaultOAuth2UserService();
 
         return ( userRequest ) -> {
-            OidcUser oidcUser = delegate.loadUser( userRequest );
-            //TODO find a way to extract roles
+            OAuth2User oAuth2User = delegate.loadUser( userRequest );
             OAuth2AccessToken accessToken = userRequest.getAccessToken();
+            Jwt jwt = JwtDecoders.fromIssuerLocation(issuer).decode(accessToken.getTokenValue());
+            //TODO parse the JWT to extract roles
             Set<GrantedAuthority> mappedAuthorities = new HashSet<>();
+          return  new DefaultOAuth2User( mappedAuthorities, oAuth2User.getAttributes(), "preferred_username" );
 
-            oidcUser = new DefaultOidcUser( mappedAuthorities, oidcUser.getIdToken(), oidcUser.getUserInfo() );
-
-            return oidcUser;
         };
     }
 
